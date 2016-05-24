f5e69e25aa732452ccca3ac8f68eec6685edcf1c
Jarvis Fosdick <jarvis@spiredigital.com>
2016-05-24 14:36:43 -0600

    make circle gesture

--- eventHandler.js
+++ eventHandler.js
@@ -1,11 +1,79 @@
+eventCollection = function() {
+	this.previousEvent = false;
+	this.current = false;
+	this.set = [];
+	this.maxLength = 200;
+	return this;
+}
+eventCollection.prototype.get = function(e) {
+	return this.set;
+}
+eventCollection.prototype.add = function(e) {
+	if (this.current) {
+		this.previousEvent = this.current;
+	}
+	if (this.set.length > this.maxLength) {
+		this.enforeMax(this.maxLength);
+	}
+	var a = {
+		 'x' : e.clientX || false
+		,'y' : e.clientY || false
+		,'el' : e.target || false
+		,'offset' : this.currentOffset
+		,'charCode' : e.charCode || false
+		,'keyCode' : e.keyCode || false
+		,'vh' : false
+	}
+	if (a.x && this.previousEvent && this.previousEvent.x) {
+		a.vh = this.vectorHeading(a,this.previousEvent);
+	}
+	this.current = a;
+	this.set.unshift(a);
+}
+eventCollection.prototype.vectorHeading = function(d1,d2) {
+	var dx = d2.x - d1.x
+	 	,dy = d2.y - d1.y;
+		if (dx != 0 && dy != 0) {
+			return Math.sign(parseInt(dx,10)) + "," + Math.sign(parseInt(dy,10));
+		} else {
+			return "";
+		}
+}
+eventCollection.prototype.enforeMax = function(max) {
+	cnt=parseInt(max/2,10);
+	this.set = this.set.splice(0,cnt);
+}
+eventCollection.prototype.remove = function(o) {
+	var ns = {}
+	if (Number.isInteger(o)) {
+		for (var x in this.set) {
+			if (x !== o) {
+				ns[x] = this.set[x];
+			}
+		}
+	} else {
+		for (var x in this.set) {
+			if (x !== o.offset) {
+				ns[x] = this.set[x];
+			}
+		}
+	}
+	this.set = ns;
+}
+
+
 eventHandler = function() {
 	"use strict";
 
 	this.clickStack = [];
-	this.eventStack = [];
+	this.eventStack = new eventCollection;
 	this.moveStack = [];
 
 	this.listen = function(e,evt) {
+		this.eventStack.add(e,evt);
+
+		this.findGesture(this.eventStack.get());
+
 		if (["CLICK", "DBLCLICK"].indexOf(evt) !== -1) {
 			this.clickStack.unshift(evt);
 			var cS = this.clickStack;
@@ -20,20 +88,27 @@ eventHandler = function() {
 			if (re.test(mS)) {
 				this.params.eventPatterns.clearLastBang();
 				this.moveStack = [];
-			}
-		}
-		this.createCase(e);
-	}
-	this.createCase = function(e) {
-		var cCase = {
-			'e1' : {
-				 'x' : e.clientX
-				,'y' : e.clientX
-				,'name' : e.target
-			}
-		}
-	}
 
+			}
+		}
+	}
+	this.findGesture = function(stack) {
+		stack = stack || this.eventStack.get();
+		var c = stack,s = "";
+		for (var x in c) {
+			if (c[x].vh != "") {
+				s += " " + c[x].vh;
+			}
+		}
+		var clockwiseRE = /( 1,1){1,}( 1,-1){1,}( -1,-1){1,}( -1,1){1,}/;
+		if (clockwiseRE.test(s)) {
+			console.log('we found a circle');
+		}
+
+	}
+	this.getStack = function() {
+		console.log(this.eventStack);
+	}
 	this.report = function(o) {
 		for (var x in this.clickStack) {
 			if (o) {
@@ -49,7 +124,7 @@ eventHandler = function() {
     	this.params = params;
 		["CLICK", "DBLCLICK", "KEYDOWN", "KEYUP", "KEYPRESS", "MOUSEDOWN", "MOUSEMOVE", "MOUSEUP", "MOUSEOUT"].forEach(function(ev) {
 			el.addEventListener(ev.toLowerCase(), function(e) {
-					that.listen(e,ev);
+				that.listen(e,ev);
 			})
 		});
 		return true;
